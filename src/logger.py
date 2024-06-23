5849148b98d5773e9a156620f31ca66916187159
unknown <anuragsunil472@gmail.com>
2024-06-23 22:40:37 +0530

    Data Ingestion and logger code update

--- src/logger.py
+++ src/logger.py
@@ -1,18 +1,20 @@
-#check logger documentation
-
 import logging
 import os
 from datetime import datetime
 
+# Generate log file name and directory name
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+log_file_name = os.path.splitext(LOG_FILE)[0]  # Remove the .log extension for directory name
+logs_dir = os.path.join(os.getcwd(), "logs", log_file_name)
+os.makedirs(logs_dir, exist_ok=True)  # Create the directory
 
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+# Path for the log file within the new directory
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
 
+# Configure logging
 logging.basicConfig(
-    filename = LOG_FILE_PATH,
-    format = "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s %(message)s",
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s %(message)s",
     level=logging.INFO,
 )
 
