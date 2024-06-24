a83c794c4be058393586f1a9f28170df2e313f5e
unknown <anuragsunil472@gmail.com>
2024-06-24 19:10:56 +0530

    Data Transformation done(Exception updated)

--- src/exception.py
+++ src/exception.py
@@ -1,26 +1,19 @@
-#check exception documentation
-
 import sys
 import logging
-from src.logger import logging
 
+# Assuming logging is imported correctly from src.logger.logging
 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb = error_detail.exc_info() # extract the useful message
-    file_name = exc_tb.tb_frame.f_code.co_filename # to get the file name
-    error_message = "Error occred in python script name [{0}] line number [{1}] error message[{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error)
-
-    )
+def error_message_detail(error, error_detail):
+    exc_type, exc_value, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = f"Error occurred in Python script: {file_name}, line number: {line_number}, error message: {str(error)}"
     return error_message
 
-# still need full understanding of this class
-
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self, error_message, error_detail):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message,error_detail=error_detail)
+        self.error_message = error_message_detail(error_message, error_detail)
 
-        def __str__(self):
-            return self.error_message
-        
+    def __str__(self):
+        return self.error_message
